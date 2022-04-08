57aa1298e10dd16b06d329955beecf64f6877cee
Buff4eyes <claudemokbel@gmail.com>
2022-04-08 10:16:57 -0700

    staff member object template

---
+++ src/components/StaffCard/StaffMembers.js
@@ -0,0 +1,60 @@
+const StaffMembers = [
+  {
+    name: 'Staff one',
+    title: 'President',
+    dateJoined: 'October 15, 2020',
+    reportsTo: '',
+    responsibilities: '',
+    img: ''
+  },
+  {
+    name: 'Staff two',
+    title: 'President',
+    dateJoined: 'October 15, 2020',
+    reportsTo: '',
+    responsibilities: '',
+    img: ''
+  },
+  {
+    name: 'Staff three',
+    title: 'President',
+    dateJoined: 'October 15, 2020',
+    reportsTo: '',
+    responsibilities: '',
+    img: ''
+  },
+  {
+    name: 'Staff four',
+    title: 'President',
+    dateJoined: 'October 15, 2020',
+    reportsTo: '',
+    responsibilities: '',
+    img: ''
+  },
+  {
+    name: 'Staff five',
+    title: 'President',
+    dateJoined: 'October 15, 2020',
+    reportsTo: '',
+    responsibilities: '',
+    img: ''
+  },
+  {
+    name: 'Staff six',
+    title: 'President',
+    dateJoined: 'October 15, 2020',
+    reportsTo: '',
+    responsibilities: '',
+    img: ''
+  },
+  {
+    name: 'Staff seven',
+    title: 'President',
+    dateJoined: 'October 15, 2020',
+    reportsTo: '',
+    responsibilities: '',
+    img: ''
+  }
+]
+
+export default StaffMembers;
