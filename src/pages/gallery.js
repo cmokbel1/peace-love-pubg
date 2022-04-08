788c98f69a2df093f73a7063658cf19f7c001a4c
Buff4eyes <claudemokbel@gmail.com>
2022-04-08 12:18:58 -0700

    icon adjustments

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -3,7 +3,9 @@ import { GalleryCard }from '../components'
 const Gallery = (props) => {
   const { tournaments } = props;
   return (
+
     <div className="tournamentRow">
+      <h1>Tournaments:</h1>
       {tournaments.map((tournament, idx) => <GalleryCard key={idx} tournament={tournament} />)}
     </div>
   )
