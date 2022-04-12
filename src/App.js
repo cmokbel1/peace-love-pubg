fdb5e3106506fc16dd7767018feb45eddf13ed83
Buff4eyes <claudemokbel@gmail.com>
2022-04-11 17:39:13 -0700

    fixed issue with router/nav

--- src/App.js
+++ src/App.js
@@ -7,33 +7,35 @@ import './App.css';
 function App() {
   return (
     <>
-    <nav>
-      <Router>
-        <div className="App">
-          <div className="logo">PLP</div>
-          <ul className="App-header">
-            <li key='home'>
-              <Link to="/">Home</Link>
-            </li>
-            <li key='gallery'>
-              <Link to="/gallery">Gallery</Link>
-            </li>
-            <li key='staff'>
-              <Link to="/staff">Staff</Link>
-            </li>
-            <li key='about'>
-              <Link to="/about">About Us</Link>
-            </li>
-          </ul>
-          <Routes>
-            <Route exact path='/' element={<Home />}></Route>
-            <Route exact path='/gallery' element={<Gallery tournaments={tournaments} />}></Route>
-            <Route exact path='/staff' element={< Staff staffMembers={staffMembers} />}></Route>
-            <Route exact path='/about' element={< About />}></Route>
+      <nav>
+        <Router>
+          <div className="App">
+            <div className="logo">PLP</div>
+            <nav>
+              <ul className="App-header">
+                <li key='home'>
+                  <Link to="/">Home</Link>
+                </li>
+                <li key='gallery'>
+                  <Link to="/gallery">Gallery</Link>
+                </li>
+                <li key='staff'>
+                  <Link to="/staff">Staff</Link>
+                </li>
+                <li key='about'>
+                  <Link to="/about">About Us</Link>
+                </li>
+              </ul>
+            </nav>
+            <Routes>
+              <Route exact path='/' element={<Home />}></Route>
+              <Route exact path='/gallery' element={<Gallery tournaments={tournaments} />}></Route>
+              <Route exact path='/staff' element={< Staff staffMembers={staffMembers} />}></Route>
+              <Route exact path='/about' element={< About />}></Route>
 
-          </Routes>
-        </div>
-      </Router>
+            </Routes>
+          </div>
+        </Router>
       </nav>
     </>
   );
